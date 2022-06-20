5f41556aeb6e1b469ab06031792871b78564c6bf
James Agbotta <52111720+james-agbotta@users.noreply.github.com>
2022-06-20 01:11:10 +0100

    Initial plans to pivot

--- Proxima_Java/src/com/proxima/model/ModelApplication.java
+++ Proxima_Java/src/com/proxima/model/ModelApplication.java
@@ -1,6 +1,11 @@
 package com.proxima.model;
 
 public class ModelApplication {
+	private  Customer customerPointer;
+	private  Driver   DriverPointer;
+	private  Hire hirePointer; 
+	private  Street mapLocation;
+	
 	
 
 }
--- Proxima_Java/src/com/proxima/model/ScheduledHire.java
+++ Proxima_Java/src/com/proxima/model/ScheduledHire.java
@@ -6,5 +6,18 @@ public class ScheduledHire extends Hire{
 	
 	private Date bookingDate;
 	private Date pickUpDate;
+	
+	
+	
+	public void setBookingDate(long bookingDate) 
+	{
+		this.bookingDate= new Date(bookingDate);
+	}
+	
+	public void setPickupDate(long pickupDate) 
+	{
+		this.pickUpDate = new Date(pickupDate);
+		
+	}
 
 }
