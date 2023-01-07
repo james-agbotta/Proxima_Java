6a776e486b02c4e42215fd5226a560a64dbc8ed4
James Agbotta <52111720+james-agbotta@users.noreply.github.com>
2023-01-07 00:27:26 +0000

    Fresh Start Part 1
    Removal of the previous Model attempt

--- Proxima_Java/src/com/proxima/model/Address.java
+++
@@ -1,7 +0,0 @@
-package com.proxima.model;
-
-public class Address {
-	private Street street;
-	private String name;
-
-}
--- Proxima_Java/src/com/proxima/model/Alert.java
+++
@@ -1,9 +0,0 @@
-package com.proxima.model;
-
-public class Alert {
-	protected String message;
-	protected int priority;
-	public static enum priorities {LOW, NORMAL, HIGH}
-	
-
-}
